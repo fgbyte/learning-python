cf658b02b724a41ba5e2c45eb4f503ac87d2cdf1
fgbyte <fer.garciabayon@gmail.com>
2025-01-03 23:27:30 -0500

    initial commit

---
+++ LESSON01/hello.py
@@ -0,0 +1,21 @@
+greetings = "Hello, World!"
+print(greetings)
+
+
+## Variables naming
+## 1. variable name can be any word
+# YES
+name = 'fgbyte ✅'
+name0001 = 'fgbyte ✅'
+_name = 'fgbyte ✅'
+varName = 'fgbyte ✅'
+
+
+# NOT
+name! = '❌'
+name-first = '❌'
+0001name = '❌'
+1name = '❌'
+## Reserve keywords
+if = '❌'
+for = '❌'
